4625229d128fd84ae53e34054bda76724ef07895
coleopter <d.farre.m@gmail.com>
2023-05-09 13:36:40 +0200

    Add raise_error param to sleep test

--- django_tasks/tasks.py
+++ django_tasks/tasks.py
@@ -1,6 +1,10 @@
 import asyncio
 
 
-async def sleep_test(duration=1):
+async def sleep_test(duration=1, raise_error=False):
     await asyncio.sleep(duration)
+
+    if raise_error:
+        raise Exception('Test error')
+
     return f"Slept for {duration} seconds"
