8ef5d07c595c1d92dfa7231b690ccec79e54237d
coleopter <d.farre.m@gmail.com>
2023-05-10 02:03:24 +0200

    FGixup

--- django_tasks/consumers.py
+++ django_tasks/consumers.py
@@ -7,15 +7,3 @@ class TaskStatusConsumer(AsyncJsonWebsocketConsumer):
     async def task_done(self, event):
         """Echoes the task-done document."""
         await self.send_json(content=event["content"])
-
-    async def connect(self):
-        # To accept the connection call:
-        await self.accept()
-
-        # Or accept the connection and specify a chosen subprotocol.
-        # A list of subprotocols specified by the connecting client
-        # will be available in self.scope['subprotocols']
-        # await self.accept("subprotocol")
-
-        # To reject the connection, call:
-        # await self.close()
