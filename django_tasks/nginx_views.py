b812f6a4a563e0637dc467666b2ef64f8e8ad190
coleopter <d.farre.m@gmail.com>
2023-09-07 03:35:09 +0200

    Remove get_proxy_url_path method

--- django_tasks/nginx_views.py
+++ django_tasks/nginx_views.py
@@ -1,7 +1,6 @@
 import os
 
 from django import http, urls, views
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
 
@@ -20,10 +19,6 @@ class NginxAccelRedirectView(views.View):
         return urls.path(os.path.join(location, '<path:path>').lstrip('/'),
                          login_required(cls.as_view(location=location)),
                          **kwargs)
-
-    @classmethod
-    def get_proxy_url_path(cls, **kwargs):
-        return cls.get_url_path(settings.CHANNEL_TASKS.proxy_route, **kwargs)
 
     def get(self, request, path, *args, **kwargs):
         response = http.HttpResponse()
