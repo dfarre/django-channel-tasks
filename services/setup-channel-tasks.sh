877ed218bf01268f9fda576d1f71612aa76a4d8f
coleopter <d.farre.m@gmail.com>
2023-11-03 23:02:17 +0100

    Use POST to add the djangochannels unit app

--- services/setup-channel-tasks.sh
+++ services/setup-channel-tasks.sh
@@ -13,5 +13,5 @@ export CHANNEL_TASKS_PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.versi
 export CHANNEL_TASKS_ASGI_PATH="${CHANNEL_TASKS_HOME}/lib/python${CHANNEL_TASKS_PYTHON_VERSION}/site-packages"
 envsubst '\$DJANGO_SETTINGS_MODULE \$DJANGO_SECRET_KEY \$CHANNEL_TASKS_USER \$CHANNEL_TASKS_DB_PASSWORD \$CHANNEL_TASKS_INI_PATH \$CHANNEL_TASKS_PYTHON_HOME \$CHANNEL_TASKS_ASGI_PATH \$CHANNEL_TASKS_STATIC_ROOT \$CHANNEL_TASKS_STATIC_URI \$CHANNEL_TASKS_LISTENER_ADDRESS \$CHANNEL_TASKS_PYTHON_VERSION \$CHANNEL_TASKS_EMAIL_USER \$CHANNEL_TASKS_EMAIL_PASSWORD' \
  < "${CHANNEL_TASKS_HOME}"/channel-tasks-docker/channel-tasks-unit.template.json > "${CHANNEL_TASKS_HOME}"/channel-tasks-unit.json
-curl -X PUT --data-binary @"${CHANNEL_TASKS_HOME}"/channel-tasks-unit.json --unix-socket /var/run/control.unit.sock http://localhost/config/
+curl -X POST --data-binary @"${CHANNEL_TASKS_HOME}"/channel-tasks-unit.json --unix-socket /var/run/control.unit.sock http://localhost/config/
 systemctl restart unit
